cd74c98aeb8643d2e0fec0043e3dbaa7df269045
Ashan Jayasanka <59844700+AshanXJay@users.noreply.github.com>
2024-01-15 08:02:46 +0530

    check enrollment function update

--- Frontend/Frontend.c
+++ Frontend/Frontend.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 // Assuming a function that checks the enrollment number in the database
 int check_enrollment(char *enroll_no) {
-    FILE *file = fopen("D:/GitHub/Hostal-Register/Students.txt", "r");
-    if (file == NULL) {
+    FILE *s_data = fopen("D:/GitHub/Hostal-Register/Students.txt", "r");
+    if (s_data == NULL) {
         printf("Error opening file\n");
         return 0;
     }
 
-    char part[50];
-    while (fscanf(file, " %[^,\n]", part) != EOF) {
-        if (strcmp(part, enroll_no) == 0) {
-            fclose(file);
+    char enroll_data[15];
+    while (fscanf(s_data, "%s", enroll_data) != EOF) {
+        if (strcmp(enroll_data, enroll_no) == 0) {
+            fclose(s_data);
             return 1;
         }
-        // Skip to the next line
-        fscanf(file, "%*[^\n]\n");
     }
 
-    fclose(file);
+    fclose(s_data);
     return 0;
 }
 
@@ -30,14 +29,25 @@ void add_record(char *enroll_no, char *destination, time_t time) {
 }
 
 int main() {
-    char enroll_no[50];
+    char enroll_input[8];
+    char enroll_no[15];
     char destination[50];
     time_t t;
 
     while(1) {
         printf("Enter student's enrollment number: ");
-        scanf("%s", enroll_no);
+        scanf("%s", enroll_input);
 
+        // Convert to uppercase
+        for(int i = 0; enroll_input[i]; i++){
+            enroll_input[i] = toupper(enroll_input[i]);
+        }
+
+        char degree[4], year[3], number[4];
+        sscanf(enroll_input, "%3s%2s%3s", degree, year, number);
+        sprintf(enroll_no, "UWU/%s/%s/%s", degree, year, number);
+        printf("%s\n", enroll_no); 
+              
         if(check_enrollment(enroll_no)) {
             printf("Enter destination: ");
             scanf("%s", destination);
