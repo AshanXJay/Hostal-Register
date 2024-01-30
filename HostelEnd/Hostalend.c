5489c5ca12e9229b0db12219e2b4693246e1e280
Ashan Jayasanka <59844700+AshanXJay@users.noreply.github.com>
2024-01-30 06:07:16 +0530

    Update hostel variable length

--- HostelEnd/Hostalend.c
+++ HostelEnd/Hostalend.c
@@ -117,7 +117,7 @@ StudentDetails fetch_student_details(char* enroll_num) {
 // Main function
 int main(){
     int hostel_no; // Variable to hold the hostel number
-    char hostel[5];
+    char hostel[6];
 
     printf("Enter the number of the hostel in duty (1 for Boys hostel, 2 for Girls' hostel): ");
     scanf("%d", &hostel_no); // Read the input from the user
